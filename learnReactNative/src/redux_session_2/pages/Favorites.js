da380fc2ad34c40c0577aaa6b9eb5b0dad5f72be
serdar <serdar83durmus@gmail.com>
2020-11-06 10:43:01 +0100

    redux_session_2 er startet

---
+++ learnReactNative/src/redux_session_2/pages/Favorites.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View} from 'react-native';
+
+const Favorites = (props) => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Favorites</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export { Favorites }
